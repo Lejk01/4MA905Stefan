591e661cff1ed52ed91014f0c7445adfe1a89335
Lejk01 <117583777+Lejk01@users.noreply.github.com>
2025-09-15 15:44:53 +0200

    Analytical solutions, convection matrix and plot

--- functions.py
+++ functions.py
@@ -1,4 +1,5 @@
 import numpy as np
+from scipy.special import erf
 
 def phi_j(j, h, x, nodes):
   x_lb = nodes[j-1]
@@ -27,7 +28,7 @@ def phi_j_dt(j, h, x, nodes):
 def construct_mass_matrix(N, h):
   M = np.zeros((N, N))
   np.fill_diagonal(M, 2*h / 3)
-  M[0, 0] = M[N-1, N-1] = h/3
+  M[0, 0] = M[N-1, N-1] = h / 3
 
   val = h / 6
   for i in range(N-1):
@@ -38,7 +39,7 @@ def construct_mass_matrix(N, h):
 def construct_stiffness_matrix(N, h):
   A = np.zeros((N, N))
   np.fill_diagonal(A, 2 / h)
-  A[0, 0] = A[N-1, N-1] = h
+  A[0, 0] = A[N-1, N-1] = 1 / h
 
   val = - 1 / h
   for i in range(N-1):
@@ -46,5 +47,18 @@ def construct_stiffness_matrix(N, h):
     A[i+1, i] = val
   return A
 
-def construct_convection_matrix():
-  pass
+def construct_convection_matrix(N):
+  K = np.zeros((N, N))
+  val = .5
+  for i in range(N-1):
+    K[i, i+1] = val
+    K[i+1, i] = -val
+
+def analytical_m(m_L, alpha_L, lambd, x, t):
+  return m_L * (1 - erf(x / (2*np.sqrt(alpha_L*t))) / erf(lambd) )
+
+def analytical_s(alpha_L, lambd, t):
+  return 2*lambd*np.sqrt(alpha_L*t)
+
+def f_lambd(lambd, c_L, m_L, l):
+  return lambd * np.exp(lambd**2) * erf(lambd) - (m_L * c_L) / (l*np.sqrt(np.pi))
--- main.py
+++ main.py
@@ -1,9 +1,33 @@
 import numpy as np
-from functions import construct_mass_matrix
+import matplotlib.pyplot as plt
+import seaborn as sns
+from functions import (construct_mass_matrix, 
+                       construct_convection_matrix, 
+                       construct_stiffness_matrix,
+                       analytical_m,
+                       analytical_s
+                       )
 
 N_nodes = 100
+HORIZON = 10000 # [0, T] = [0, HORIZON]
+l = 2
+k_L = 0.5
+p_L = 0.5
+m_L = 10
+c_L = 0.5
 space = np.linspace(start=0, stop=1, num=10_000, endpoint=True)
+time = np.linspace(start=0, stop=HORIZON, num=10_000, endpoint=True)
 h = 0.1
+alpha_L = k_L / (p_L * c_L)
+lambd = 0.5
 nodes = np.arange(0, N_nodes, step=h)
 
-print(construct_mass_matrix(N_nodes, h))
+s = np.array([analytical_s(alpha_L, lambd, t) for t in time])
+
+sns.set_theme(style="whitegrid")
+plt.figure(figsize=(8, 4))
+sns.lineplot(x=time, y=s)
+plt.xlabel("Time")
+plt.ylabel("s(t)")
+plt.title("Interface position in x dimension")
+plt.show()
