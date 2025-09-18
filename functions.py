ef4eb8fda0baeea577436ef9f03169eb6c2fd081
Lejk01 <117583777+Lejk01@users.noreply.github.com>
2025-09-18 15:29:44 +0200

    Not functional

--- functions.py
+++ functions.py
@@ -4,8 +4,11 @@ from scipy.special import erf
 def phi_j(j, h, x, nodes):
   x_lb = nodes[j-1]
   x_j = nodes[j]
-  x_ub = nodes[j+1]
-  
+  if j+1 < len(nodes): # Nåt fel här.
+    x_ub = nodes[j+1]
+  else:
+    return 1
+
   if x >= x_lb  and x <= x_j:
     return (x - x_lb) / h
   elif x >= x_j and x <= x_ub:
@@ -13,6 +16,21 @@ def phi_j(j, h, x, nodes):
   else:
     return 0
   
+def phi_j_vectorized(j, h, x, nodes):
+  x_lb = nodes[j - 1] if j - 1 >= 0 else nodes[j]
+  x_j = nodes[j]
+  x_ub = nodes[j + 1] if j + 1 < len(nodes) else nodes[j]
+
+  res = np.zeros_like(x, dtype=float)
+
+  mask_left = (x >= x_lb) & (x <= x_j)
+  res[mask_left] = (x[mask_left] - x_lb) / h
+
+  mask_right = (x >= x_j) & (x <= x_ub)
+  res[mask_right] = (x_ub - x[mask_right]) / h
+  
+  return res
+
 def phi_j_dt(j, h, x, nodes):
   x_lb = nodes[j-1]
   x_j = nodes[j]
@@ -28,7 +46,7 @@ def phi_j_dt(j, h, x, nodes):
 def construct_mass_matrix(N, h):
   M = np.zeros((N, N))
   np.fill_diagonal(M, 2*h / 3)
-  M[0, 0] = M[N-1, N-1] = h / 3
+  M[0, 0] = M[-1, -1] = h / 3
 
   val = h / 6
   for i in range(N-1):
@@ -47,12 +65,18 @@ def construct_stiffness_matrix(N, h):
     A[i+1, i] = val
   return A
 
-def construct_convection_matrix(N):
+def construct_convection_matrix(N, h, nodes):
   K = np.zeros((N, N))
-  val = .5
+  K[0, 0] = -(nodes[1]**3 - nodes[0]**2 * (3*nodes[1] - 2*nodes[0])) / (6*h**2)
+  K[-1, -1] = (nodes[-2]**3 - nodes[-1]**2 * (3*nodes[-2] - 2*nodes[-1])) / (6*h**2)
+
   for i in range(N-1):
-    K[i, i+1] = val
-    K[i+1, i] = -val
+    if i > 0 and i < N-1:
+      K[i, i] = ((nodes[i+1]**3 - nodes[i-1]**3)/6 - nodes[i]**2 * h) / h**2
+    K[i, i+1] =  (nodes[i+1]**3 - nodes[i]**2 * (3*nodes[i+1] - 2*nodes[i])) / (6*h**2)
+    K[i+1, i] = -(nodes[i]**3 - nodes[i+1]**2 * (3*nodes[i] - 2*nodes[i+1])) / (6*h**2)
+  
+  return K
 
 def analytical_m(m_L, alpha_L, lambd, x, t):
   return m_L * (1 - erf(x / (2*np.sqrt(alpha_L*t))) / erf(lambd) )
@@ -62,3 +86,37 @@ def analytical_s(alpha_L, lambd, t):
 
 def lambdeq(lambd, c_L, m_L, l):
   return lambd * np.exp(lambd**2) * erf(lambd) - (m_L * c_L) / (l*np.sqrt(np.pi))
+
+def finite_diff(heat_distribution, interface, n, h, space):
+  x_domain_idx = np.where(space < interface[n])[0]  # enforce s 0 < x < s(t).
+  m = heat_distribution[n, x_domain_idx] # grab m with support on x.
+  dmdx = np.zeros_like(m) 
+
+  # interior points.
+  dmdx[1:-1] = (m[2:] - m[:-2]) / (2 * h)
+
+  # one-sided for boundaries.
+  dmdx[0] = (m[1] - m[0]) / h
+  dmdx[-1] = (m[-1] - m[-2]) / h
+
+  # scale to xi-space and return dmdxi at s(t) <=> xi = 1.
+  dmdxi = interface[n] * dmdx
+  return dmdxi[dmdxi < 0][-1]
+
+def finite_diff_xi(heat_distribution, n, h):
+  m_tilde = heat_distribution[n, :] # m_tilde()
+  # last two points: one-sided difference
+  dmdxi_interior = np.diff(m_tilde[1:-1]) / (2*h)
+  print("dmdxi_interior=", dmdxi_interior)
+
+  dmdxi_start = (m_tilde[1] - m_tilde[0]) / h
+  print("dmdxi_start=", dmdxi_start)
+
+  dmdxi_end = (m_tilde[-1] - m_tilde[-2]) / h
+  print("dmdxi_end=", dmdxi_end)
+
+  dmdxi = np.sum(dmdxi_interior) + dmdxi_start + dmdxi_end
+  print("dmdxi=", dmdxi)
+
+  return dmdxi
+  
--- main.py
+++ main.py
@@ -6,28 +6,38 @@ from functions import (construct_convection_matrix,
                        construct_stiffness_matrix,
                        analytical_m, 
                        analytical_s,
-                       lambdeq
+                       lambdeq,
+                       phi_j,
+                       phi_j_vectorized,
+                       finite_diff_xi,
+                       finite_diff
                        )
 from scipy.optimize import fsolve
+from functools import partial
 
 # Water–ice constants.
-k_L = 0.6         # W/(mK)
-rho_L = 1000      # kg/m^3
-c_L = 4200        # J/(kgK)
-m_L = 334e3       # J/kg
+k_L = 0.5664         # W/(mK)
+rho_L = 1000        # kg/m^3
+c_L = 4189.9        # J/(kgK)
+m_L = 298.15
 alpha_L = k_L / (rho_L * c_L)  # m^2/s
-l = 10            # temperature difference driving phase change [K]
-
-lambda_guess = 0.5 # Shit guess probably?
+l = 334e3             # temperature difference driving phase change [K]
+N_nodes = 300
+L = 0.1
+lambda_guess = 0.5
 
 lambd = fsolve(lambdeq, lambda_guess, args=(c_L, m_L, l))[0]
+nodes = np.linspace(0, 1, N_nodes)
+h_nodes = nodes[1] - nodes[0]
 
 # Time horizon (seconds).
-HORIZON = 600     # 10 minutes
-time = np.linspace(1e-6, HORIZON, 300)  # 300 time steps
+HORIZON = 600     # 5 minutes
+time = np.linspace(1e-6, HORIZON, 1000)
+dt = time[1] - time[0]
+space = np.linspace(0, L, 1000) 
+h_space = space[1] - space[0]
 
-space = np.linspace(0, 0.05, 300) 
-
+### Analytical solution - Start ###
 s = analytical_s(alpha_L, lambd, time)
 
 T, X = np.meshgrid(time, space, indexing="ij")
@@ -35,6 +45,56 @@ S = analytical_s(alpha_L, lambd, T)
 M = analytical_m(m_L, alpha_L, lambd, X, T)
 
 M[X > S] = np.nan
+
+### Analytical solution - End ###
+
+## Finite element method Uniform mesh - Start ##
+
+mass_matrix = construct_mass_matrix(N_nodes, h_nodes)
+stiffnes_matrix = construct_stiffness_matrix(N_nodes, h_nodes)
+convection_matrix = construct_convection_matrix(N_nodes, h_nodes, nodes)
+
+heat_distribution_x = np.zeros((len(time), len(space))) # m(x,t)
+heat_distribution_x[:, 0] = m_L # m_L at the intial point for all times. 
+
+initial_heat_distribution_xi = np.zeros((len(time), N_nodes)) # mtilde(xi,t)
+initial_heat_distribution_xi[:, 0] = m_L # m_L at the intial point for all times. 
+interface = np.zeros(len(time)) # this is s(t), so the position of the interface at each time point.
+interface[0] = h_nodes # The position of the interface is close to 0 at t=0. This is s(0) = s_0 >= 0
+
+# a coefficients
+a = np.zeros(N_nodes)
+a[0] = m_L
+
+# initial condition:
+initial_condition = np.zeros(N_nodes)
+initial_condition[0] = m_L
+
+# Solve problem for each time point:
+for n in range(len(time)-1):
+  dmdxi = finite_diff(heat_distribution_x, interface, n, h_space, space)
+  s_next = interface[n] - dmdxi * dt * k_L/(rho_L*l*interface[n])
+  interface[n+1] = s_next
+  a = np.linalg.solve(mass_matrix/h_nodes - (1/s_next)*(s_next - interface[n] )/h_nodes * convection_matrix + alpha_L / s_next**2 * stiffnes_matrix, np.dot(a, mass_matrix/h_nodes) -  initial_condition)
+  
+  phis_evaluated = np.zeros((N_nodes, len(space)))
+
+  for i in range(N_nodes): # For each function phi.
+    for j, x in enumerate(space): # For each point in space.
+      phis_evaluated[i, j] = phi_j(i, h_nodes, x, nodes) # evaluate every phi on every point in space. 
+
+
+  for idx in range(len(space)):
+    heat_distribution_x[n, idx] = np.dot(a, phis_evaluated[:, idx])
+  
+  if n % 100 == 0:
+    print("At n=", n, " Interface at x=", interface[interface > 0][-1])
+    print("Len interface=", len(interface[interface > 0]))
+    print("heat ", heat_distribution_x[n, 5])
+
+## Finite element method Uniform mesh - End ##
+
+### Plots - Start ###
 
 # Plot interface s(t)
 sns.set_theme(style="whitegrid")
@@ -56,3 +116,5 @@ plt.xlabel("Time t [s]")
 plt.ylabel("Space x [m]")
 plt.title("Heat distribution m(x,t) (time on x-axis)")
 plt.show()
+
+### Plots - End ###
